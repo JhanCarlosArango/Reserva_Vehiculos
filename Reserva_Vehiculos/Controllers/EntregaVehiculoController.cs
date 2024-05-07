6739407afb138885ba56e307a45aee56b4e6b4b1
JhanCarlosArango <Parkourfree@outlook.com>
2024-05-07 13:34:32 -0500

    no se usa la vista de entrega vehiculo

--- Reserva_Vehiculos/Controllers/EntregaVehiculoController.cs
+++ Reserva_Vehiculos/Controllers/EntregaVehiculoController.cs
@@ -8,7 +8,7 @@ namespace FrontEnd.Controllers
         Vehiculo_DAO vehiculo_DAO;
         public IActionResult EntregaVehiculo()
         {
-            vehiculo_DAO = new Vehiculo_DAO();
+           vehiculo_DAO = new Vehiculo_DAO();
             return View(vehiculo_DAO);
         }
     }
